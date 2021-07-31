06cd83bdacce5aebd2a954b646b1feb253b1d0c6
Quang Vo <qtvo91@yahoo.com>
2021-07-31 12:35:37 -0700

    Update after moving the test files to the test_data folder.

--- panda_cvs_demo.py
+++ panda_cvs_demo.py
@@ -1,8 +1,8 @@
 import pandas as pd
-pf=pd.read_csv("utilities/stock_data.csv", header=1, na_values=
+pf=pd.read_csv("test_data/stock_data.csv", header=1, na_values=
     {'eps': ['not available', 'n.a.'],
      'revenue': ['not available', 'n.a.',-1]
 
     })
-pf.to_csv('utilities/new_stock_data.csv', index=False, columns=['tickers', 'eps'])
+pf.to_csv('test_data/new_stock_data.csv', index=False, columns=['tickers', 'eps'])
 
--- panda_excel_demo.py
+++ panda_excel_demo.py
@@ -4,5 +4,5 @@ def convert_people_cell(cell):
     if cell=='n.a.':
         return 'sam walton'
     return cell
-pf=pd.read_excel("utilities/stock_data.xlsx", "Sheet1", converters= {'people': convert_people_cell})
-pf.to_excel("utilities/new_stock_data.xlsx", sheet_name="stocks", startrow=0, startcol=2, index=False)
+pf=pd.read_excel("test_data/stock_data.xlsx", "Sheet1", converters= {'people': convert_people_cell})
+pf.to_excel("test_data/new_stock_data.xlsx", sheet_name="stocks", startrow=0, startcol=2, index=False)
